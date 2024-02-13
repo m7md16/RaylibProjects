cb8702b9223fa1a34ee8285294302f103cf63c61
m7md16 <m7md1695@gmail.com>
2024-02-13 07:22:03 +0300

    first commit

---
+++ Axe Game/axe_game.cpp
@@ -0,0 +1,16 @@
+#include "raylib.h"
+
+int main ()
+{
+    int screenWidth = 350; 
+    int screenHieght = 350; 
+    InitWindow(screenWidth,200,"New Window");
+    while(true){
+
+        BeginDrawing();
+        
+        ClearBackground(RED);
+        EndDrawing();
+
+    }
+}
