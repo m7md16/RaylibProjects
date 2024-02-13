f1a26448ab68443a3da120aae4e2ca3bc0e111b8
m7md16 <m7md1695@gmail.com>
2024-02-13 17:30:49 +0300

    add Circle

--- Axe Game/axe_game.cpp
+++ Axe Game/axe_game.cpp
@@ -2,16 +2,28 @@
 
 int main ()
 {
-    int screenWidth = 350; 
-    int screenHieght = 350; 
-    InitWindow(screenWidth,200,"New Window");
-    //new
-    
+    //windows dimentions
+    int screenWidth {350} , screenHeight {200}; 
+    InitWindow(screenWidth,screenHeight,"New Window");
+
+    // cirecle corrdinates
+    int cirecle_x{screenWidth/2}, cirecle_y {screenHeight/2};
+
+    SetTargetFPS(60);    
     while(!WindowShouldClose()){
 
         BeginDrawing();
-        ClearBackground(RED);
-        EndDrawing();
+        ClearBackground(WHITE);
 
+        // Game logic begins
+        DrawCircle(cirecle_x, cirecle_y, 24, BLUE);
+        if(IsKeyDown(KEY_D) && cirecle_x < screenWidth) { cirecle_x += 10; } 
+        if(IsKeyDown(KEY_A) && cirecle_x > 0) { cirecle_x -= 10; } 
+        if(IsKeyDown(KEY_S) && cirecle_y < screenHeight) { cirecle_y += 10; }
+        if(IsKeyDown(KEY_W) && cirecle_y > 0) { cirecle_y -= 10; } 
+ 
+
+        // Game logic ends
+        EndDrawing();
     }
 }
