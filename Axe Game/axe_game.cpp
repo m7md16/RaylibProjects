7235df2d76349cdb3628dbcc5ace8e7086d15e2f
m7md16 <m7md1695@gmail.com>
2024-02-13 17:43:32 +0300

    Drawing the axe

--- Axe Game/axe_game.cpp
+++ Axe Game/axe_game.cpp
@@ -3,11 +3,14 @@
 int main ()
 {
     //windows dimentions
-    int screenWidth {350} , screenHeight {200}; 
+    int screenWidth {800} , screenHeight {450}; 
     InitWindow(screenWidth,screenHeight,"New Window");
 
-    // cirecle corrdinates
-    int cirecle_x{screenWidth/2}, cirecle_y {screenHeight/2};
+    // circle coordinates
+    int circle_x{screenWidth/2}, circle_y{screenHeight/2};
+
+    // axe coordinate
+    int axe_x{400}, axe_y{0};
 
     SetTargetFPS(60);    
     while(!WindowShouldClose()){
@@ -16,11 +19,17 @@ int main ()
         ClearBackground(WHITE);
 
         // Game logic begins
-        DrawCircle(cirecle_x, cirecle_y, 24, BLUE);
-        if(IsKeyDown(KEY_D) && cirecle_x < screenWidth) { cirecle_x += 10; } 
-        if(IsKeyDown(KEY_A) && cirecle_x > 0) { cirecle_x -= 10; } 
-        if(IsKeyDown(KEY_S) && cirecle_y < screenHeight) { cirecle_y += 10; }
-        if(IsKeyDown(KEY_W) && cirecle_y > 0) { cirecle_y -= 10; } 
+        DrawCircle(circle_x, circle_y, 24, BLUE);
+        DrawRectangle(axe_x, axe_y, 50, 50, RED);
+
+        // move axe
+        axe_y += 5;
+        if(axe_y == screenHeight) {axe_y = 0;}
+        // Circle movement
+        if(IsKeyDown(KEY_D) && circle_x < screenWidth) { circle_x += 10; } 
+        if(IsKeyDown(KEY_A) && circle_x > 0) { circle_x -= 10; } 
+        if(IsKeyDown(KEY_S) && circle_y < screenHeight) { circle_y += 10; }
+        if(IsKeyDown(KEY_W) && circle_y > 0) { circle_y -= 10; } 
  
 
         // Game logic ends
