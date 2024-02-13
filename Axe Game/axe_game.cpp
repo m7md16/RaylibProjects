3fb8b5a63f9f74d6e218f8cf53b346888cd8bba6
m7md16 <m7md1695@gmail.com>
2024-02-13 07:35:27 +0300

    a new comment add

--- Axe Game/axe_game.cpp
+++ Axe Game/axe_game.cpp
@@ -5,10 +5,12 @@ int main ()
     int screenWidth = 350; 
     int screenHieght = 350; 
     InitWindow(screenWidth,200,"New Window");
+    
+    //keep the window appear
+    
     while(true){
 
         BeginDrawing();
-        
         ClearBackground(RED);
         EndDrawing();
 
